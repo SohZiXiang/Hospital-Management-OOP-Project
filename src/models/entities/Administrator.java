9f5f410974d35e828f52c1219490cc9917536663
HX987 <leehsienx@gmail.com>
2024-10-18 17:40:15 +0800

    add basic skeleton code to administator

--- src/models/entities/Administrator.java
+++ src/models/entities/Administrator.java
@@ -8,7 +8,71 @@ public class Administrator extends Staff{
     }
 
     @Override
-    public String getRole() {
+    public String getRole(){
         return "administrator";
+    }
+
+    public void addDoctor(String hospitalID, String staffId, String name, Gender gender, int age){
+        Doctor newDoctor = new Doctor(hospitalID, staffId, name, gender, age);
+    }
+
+    public void updateDoctor(Doctor doctor){
+
+    }
+
+    public void removeDoctor(Doctor doctor){
+
+    }
+
+    public void addPharmacists(){
+
+    }
+
+    public void updatePharmacists(){
+
+    }
+
+    public void removePharmacists(){
+
+    }
+
+    public void retrieveAppointment(Appointment appointment) {
+        System.out.println("Viewing appointment records for: " + appointment.getAppointmentId());
+        System.out.println("Patient ID: " + appointment.getPatientId());
+        System.out.println("Doctor ID: " + appointment.getDoctorId());
+        System.out.println("Appointment Status: " + appointment.getStatus());
+        System.out.println("Appointment Date: " + appointment.getAppointmentDate());
+        System.out.println("Appointment Time: " + appointment.getAppointmentTime());
+        System.out.println("Appointment Outcome Record: " + appointment.getOutcomeRecord());
+    }
+
+    public void updateMedicineStock(Medicine medicine, int stock){
+        medicine.setStock(stock);
+    }
+
+    public void removeMedicineStock(Medicine medicine, int stock){
+        int currentStock = medicine.getStock();
+
+        if(currentStock - stock > 0){
+            currentStock = currentStock - stock;
+        }
+        else{
+            currentStock = 0;
+        }
+
+        medicine.setStock(currentStock);
+    }
+
+    public void addMedicineStock(Medicine medicine, int stock){
+        int currentStock = medicine.getStock();
+        medicine.setStock(currentStock + stock);
+    }
+
+    public void updateMedicineLowStockLevelAlert(Medicine medicine, int stock){
+        medicine.setLowStockAlert(stock);
+    }
+
+    public void approveReplenishmentRequest(){
+
     }
 }
