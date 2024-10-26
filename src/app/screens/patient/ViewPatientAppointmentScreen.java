127524de412b9ff24433a2372d50b98c763ed767
HX987 <leehsienx@gmail.com>
2024-10-26 22:51:23 +0800

    patient able to view appointment

--- src/app/screens/patient/ViewPatientAppointmentScreen.java
+++ src/app/screens/patient/ViewPatientAppointmentScreen.java
@@ -1,54 +1,62 @@
 package app.screens.patient;
 
+import app.loaders.AppointmentLoader;
+import app.loaders.StaffLoader;
+import interfaces.DataLoader;
 import interfaces.Screen;
+import models.entities.Appointment;
+import models.entities.Staff;
 import models.entities.User;
+import models.enums.FilePaths;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ViewPatientAppointmentScreen implements Screen {
 
-//    DataLoader appointmentLoader = new AppointmentLoader();
-//    List<Appointment> appointmentList = new ArrayList<>();
-//    String appointmentPath = FilePaths.APP_DATA.getPath();
-//
-//    DataLoader staffLoader = new StaffLoader();
-//    List<Staff> staffList = new ArrayList<>();
-//    String staffPath = FilePaths.STAFF_DATA.getPath();
+    DataLoader appointmentLoader = new AppointmentLoader();
+    List<Appointment> appointmentList = new ArrayList<>();
+    String appointmentPath = FilePaths.APPT_DATA.getPath();
+
+    DataLoader staffLoader = new StaffLoader();
+    List<Staff> staffList = new ArrayList<>();
+    String staffPath = FilePaths.STAFF_DATA.getPath();
 
     @Override
     public void display(Scanner scanner, User user) {
 
-//        System.out.println("Displaying Appointments for patient: " + user.getName());
-//
-//        try {
-//            appointmentList = appointmentLoader.loadData(appointmentPath);
-//        }
-//        catch (Exception e) {
-//            System.err.println("Error loading data: " + e.getMessage());
-//        }
-//
-//        try {
-//            staffList = staffLoader.loadData(staffPath);
-//        }
-//        catch (Exception e) {
-//            System.err.println("Error loading data: " + e.getMessage());
-//        }
-//
-//        for (Appointment appointment : appointmentList) {
-//            if (appointment.getPatientId().equals(user.getHospitalID())) {
-//                System.out.println("Appointment ID: " + appointment.getAppointmentId());
-//
-//                for (Staff staff : staffList) {
-//                    if (staff.getStaffId().equals(appointment.getDoctorId())) {
-//                        System.out.println("Doctor: Dr " + staff.getName());
-//                    }
-//                }
-//
-//                System.out.println("Appointment Status: " + appointment.getStatus());
-//                System.out.println("Appointment Date: " + appointment.getAppointmentDate());
-//                System.out.println("Appointment Time: " + appointment.getAppointmentTime());
-//            }
-//        }
+        System.out.println("Displaying Appointments for patient: " + user.getName());
+
+        try {
+            appointmentList = appointmentLoader.loadData(appointmentPath);
+        }
+        catch (Exception e) {
+            System.err.println("Error loading data: " + e.getMessage());
+        }
+
+        try {
+            staffList = staffLoader.loadData(staffPath);
+        }
+        catch (Exception e) {
+            System.err.println("Error loading data: " + e.getMessage());
+        }
+
+        for (Appointment appointment : appointmentList) {
+            if (appointment.getPatientId().equals(user.getHospitalID())) {
+                System.out.println("Appointment ID: " + appointment.getAppointmentId());
+
+                for (Staff staff : staffList) {
+                    if (staff.getStaffId().equals(appointment.getDoctorId())) {
+                        System.out.println("Doctor: Dr " + staff.getName());
+                    }
+                }
+
+                System.out.println("Appointment Status: " + appointment.getStatus());
+                System.out.println("Appointment Date: " + appointment.getAppointmentDate());
+                System.out.println("Appointment Time: " + appointment.getAppointmentTime());
+            }
+        }
 
     }
 }
